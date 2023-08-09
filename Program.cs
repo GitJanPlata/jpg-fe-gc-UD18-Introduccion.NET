a29b2593d2cfb1dc5f8dcc01282d11e302435f34
Dorten <96839905+GitJanPlata@users.noreply.github.com>
2023-08-09 15:55:53 +0200

    Acabado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AplicacionDemo
+{
+    class Programa1
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hola Mundo!!!");
+            int num1 = 5;
+            int num2 = 6;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + num2;
+
+            Console.WriteLine("El resultado de la suma és: " + suma);
+
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El valor introducido es " + valor1);
+            int valorint = Convert.ToInt32(valor1);
+        }
+    }
+}
